d39af49d02b493475aafb1c84c0cdd3e5d76772b
Idaho Edokpayi <idaho@tridentprofessionalservices.com>
2020-04-09 09:36:57 -0400

    basic headline

---
+++ src/components/ProductLandingPage/ProductHeadline/IProductHeadlineProps.ts
@@ -0,0 +1,10 @@
+export interface IProductHeadlineProps{ 
+    buttonVisible:boolean;
+    buttonContent?:JSX.Element;
+    buttonAction?:(event:React.MouseEvent<HTMLAnchorElement, MouseEvent>)=>void;
+    buttonHref?:string;
+    headline:string;
+    headlineContent?:JSX.Element;
+    secondaryHeadlineContent?:JSX.Element;
+    subheading?:string;
+}
---
+++ src/components/ProductLandingPage/ProductHeadline/index.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import "bootstrap/scss/bootstrap.scss";
+import "../product.css";
+import { IProductHeadlineProps } from "./IProductHeadlineProps";
+
+export function ProductHeadline(props: IProductHeadlineProps): JSX.Element {
+  const {
+    buttonAction,
+    buttonContent,
+    buttonHref,
+    buttonVisible,
+    headline,
+    headlineContent,
+    secondaryHeadlineContent,
+    subheading,
+  } = props;
+  const buttonClass = buttonVisible ? "btn btn-outline-secondary" : "d-none";
+  return (
+    <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+      <div className="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 className="display-4 font-weight-normal">{headline}</h1>
+        <p className="lead font-weight-normal">{subheading}</p>
+        <a className={buttonClass} href={buttonHref} onClick={buttonAction}>
+          {buttonContent}
+        </a>
+      </div>
+      <div className="product-device shadow-sm d-none d-md-block">
+        {headlineContent}
+      </div>
+      <div className="product-device product-device-2 shadow-sm d-none d-md-block">
+        {secondaryHeadlineContent}
+      </div>
+    </div>
+  );
+}
