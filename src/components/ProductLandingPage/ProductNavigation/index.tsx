49f295e0e59fa4a1ae8067937d642e9d272e9e8c
utdcometsoccer <idaho@edokpayi.com>
2020-04-10 12:54:22 -0400

    readme files improvement

--- src/components/ProductLandingPage/ProductNavigation/index.tsx
+++ src/components/ProductLandingPage/ProductNavigation/index.tsx
@@ -5,36 +5,19 @@ import "../product.css";
 import { INavigationLink } from "./ProductMenuItem/INavigationLink";
 import { ProductMenuItem } from "./ProductMenuItem/index";
 
-export function ProductNavigation(props:IProductNavigationProps){
-    const { logo, navigation } = props;
-    const navigationItems = navigation || [];
-  return <nav className="site-header sticky-top py-1">
-  <div className="container d-flex flex-column flex-md-row justify-content-between">
-    <a className="py-2" href="#" aria-label="Product">
-      {logo}
-    </a>
-    {navigationItems.map((item:INavigationLink, index:number)=><ProductMenuItem {...item} key={`product-menu-${index}`} />)}
-    {/*<a className="py-2 d-none d-md-inline-block" href="#">
-      Tour
-     </a>
-    <a className="py-2 d-none d-md-inline-block" href="#">
-      Product
-    </a>
-    <a className="py-2 d-none d-md-inline-block" href="#">
-      Features
-    </a>
-    <a className="py-2 d-none d-md-inline-block" href="#">
-      Enterprise
-    </a>
-    <a className="py-2 d-none d-md-inline-block" href="#">
-      Support
-    </a>
-    <a className="py-2 d-none d-md-inline-block" href="#">
-      Pricing
-    </a>
-    <a className="py-2 d-none d-md-inline-block" href="#">
-      Cart
-    </a>*/}
-  </div>
-</nav>  
+export function ProductNavigation(props: IProductNavigationProps) {
+  const { logo, navigation } = props;
+  const navigationItems = navigation || [];
+  return (
+    <nav className="site-header sticky-top py-1">
+      <div className="container d-flex flex-column flex-md-row justify-content-between">
+        <a className="py-2" href="#" aria-label="Product">
+          {logo}
+        </a>
+        {navigationItems.map((item: INavigationLink, index: number) => (
+          <ProductMenuItem {...item} key={`product-menu-${index}`} />
+        ))}
+      </div>
+    </nav>
+  );
 }
