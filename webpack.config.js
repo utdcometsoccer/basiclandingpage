81a611fa7e3ca518caddd3f89407158a38d53c07
utdcometsoccer <idaho@edokpayi.com>
2020-04-11 23:13:17 -0400

    corrected entry points in build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,37 @@
-var path =require('path');
+var path = require("path");
 
 module.exports = {
   entry: {
-    "basic-landing-page": "./src/index.tsx",
+    components: [
+      "./src/components/CorporateHomePage/CorporateFooter/CorporateFooterNavigationMenu/index.tsx",
+      "./src/components/CorporateHomePage/CorporateFooter/index.tsx",
+      "./src/components/CorporateHomePage/CorporateHeader/index.tsx",
+      "./src/components/CorporateHomePage/CorporateNavigation/index.tsx",
+      "./src/components/CorporateHomePage/CorporateNavigation/NavigationItem/index.tsx",
+      "./src/components/CorporateHomePage/CorporateServices/CorporateService/index.tsx",
+      "./src/components/CorporateHomePage/CorporateServices/index.tsx",
+      "./src/components/CorporateHomePage/CorporateTestimonials/CorporateTestimonial/index.tsx",
+      "./src/components/CorporateHomePage/CorporateTestimonials/index.tsx",
+      "./src/components/CorporateHomePage/index.tsx",
+      "./src/components/ProductLandingPage/DarkProductHeadline/index.tsx",
+      "./src/components/ProductLandingPage/index.tsx",
+      "./src/components/ProductLandingPage/LightProductHeadline/index.tsx",
+      "./src/components/ProductLandingPage/ProductFooter/index.tsx",
+      "./src/components/ProductLandingPage/ProductFooter/ProductFooterNavigation/index.tsx",
+      "./src/components/ProductLandingPage/ProductFooter/ProductFooterNavigation/ProductFooterNavigationElement/index.tsx",
+      "./src/components/ProductLandingPage/ProductFooter/ProductFooterNavigation/ProductFooterNavigationElement/ProductFooterNavigationSubMenu/index.tsx",
+      "./src/components/ProductLandingPage/ProductFooter/ProductFooterNavigation/ProductFooterNavigationElement/ProductFooterNavigationSubMenu/SubMenuElement/index.tsx",
+      "./src/components/ProductLandingPage/ProductHeadline/index.tsx",
+      "./src/components/ProductLandingPage/ProductHeadLineContainer/index.tsx",
+      "./src/components/ProductLandingPage/ProductNavigation/index.tsx",
+      "./src/components/ProductLandingPage/ProductNavigation/ProductMenuItem/index.tsx",
+    ]
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
     library: "basicLandingPage",
+    libraryTarget: "umd",
   },
   module: {
     rules: [
@@ -38,5 +62,5 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
-  devtool: "source-map"
+  devtool: "source-map",
 };
