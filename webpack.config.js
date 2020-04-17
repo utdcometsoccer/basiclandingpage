df185d0773094f6c1de24bcdce0c3d2e20a3e9bf
utdcometsoccer <idaho@edokpayi.com>
2020-04-17 11:47:43 -0400

    remove umd targeting

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-    library: "basicLandingPage",
-    libraryTarget: 'umd'
+    library: "basicLandingPage"
   },
   module: {
     rules: [
