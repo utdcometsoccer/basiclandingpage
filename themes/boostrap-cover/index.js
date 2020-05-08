287f1c61d473d96750218669c6575e9950b83116
utdcometsoccer <idaho@edokpayi.com>
2020-05-07 20:12:14 -0400

    export from index.js in bootstrap cover

--- themes/boostrap-cover/index.js
+++ themes/boostrap-cover/index.js
@@ -1,1 +1,18 @@
-// do nothing 
+/// <reference types="react" />
+
+import {
+    CoverNavigation,
+    CoverPage,
+    CoverTitle,
+  } from "@idahoedokpayi/basic-landing-page-components";
+  
+  import { Layout } from "./src/components/layout";
+  import IndexPage from "./src/pages/";
+  export {
+    CoverNavigation,
+    CoverPage,
+    CoverTitle,
+    IndexPage,
+    Layout
+  };
+  
