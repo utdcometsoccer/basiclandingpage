6afeb5377514e76baeb88eaa4bc51f755f5f25d6
utdcometsoccer <idaho@edokpayi.com>
2020-05-07 20:45:03 -0400

    export from index.js in corporate home page theme

--- themes/corporate-homepage/index.js
+++ themes/corporate-homepage/index.js
@@ -1,1 +1,30 @@
-// do nothing 
+import {
+  CorporateHeader,
+  CorporateServices,
+  CorporateTestimonials,
+} from "@idahoedokpayi/basic-landing-page-components";
+
+import IndexPage from "./src/pages";
+import { Layout } from "./src/components/Layout";
+import bg from "./src/img/business-district.jpg";
+import pic1 from "./src/img/pic1.jpg";
+import pic2 from "./src/img/pic2.jpg";
+import logo from "./src/img/trident-color.svg";
+import NotFoundPage from "./src/pages/404";
+import AboutPage from "./src/pages/about";
+import ContactPage from "./src/pages/contact";
+
+export {
+  AboutPage,
+  bg,
+  ContactPage,
+  pic1,
+  pic2,
+  CorporateHeader,
+  CorporateServices,
+  CorporateTestimonials,
+  IndexPage,
+  Layout,
+  logo,
+  NotFoundPage,
+};
