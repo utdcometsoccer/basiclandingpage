9a01c6b8d35daebf870d474742d0a0e2af7b645c
utdcometsoccer <idaho@edokpayi.com>
2020-05-15 09:33:37 -0400

    bootstrap album work

--- themes/bootstrap-album/src/components/layout/index.tsx
+++ themes/bootstrap-album/src/components/layout/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link, StaticQuery, graphql } from "gatsby";
+import { Link } from "gatsby";
 import { Github } from "@idahoedokpayi/basic-landing-page-svg";
 import {
   AlbumHeader,
